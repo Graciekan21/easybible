7ad9925c457cb89d6b7708c12af3f437d99af756
Graciekan21 <>
2024-02-08 20:59:56 +0000

    Add Javascript code for click listener for answers

--- assets/script/app.js
+++ assets/script/app.js
@@ -6,6 +6,19 @@ window.onload = function () {
     //loop through each question card
     for (let i = 0; i < questions.length; i++) {
         let myQ = questions[i];
-        //
+        //collect all answers under question on index i
+        let myAnswers = myQ.querySelectorAll("input[type='radio']");
+        //loop through each answer for the selected question
+        for (let j = 0; j < myAnswers.length; j++) {
+            let myAns = myAnswers[j];
+            //add a click listener to the parent element of input the tag
+            myAns.parentNode.addEventListener("click", function () {
+                myAns.click();
+            });
+            //add a click listener to the answer element only
+            myAns.addEventListener("click", function () {
+                alert(myAns.value);
+            });
+        }
     }
 };
